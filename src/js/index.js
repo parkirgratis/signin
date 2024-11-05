4d30af1f27271a67c93c2ee30869298227ea0887
hrisz <harissaefuloh@gmail.com>
2024-11-05 21:17:07 +0700

    add: pop up for camera location screenshot

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/whatsauth.js";
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
 
+import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js";
+import {addCSS} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.9/element.js";
+
+addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
+
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 //wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTgwMDAwNjAwMD90ZXh0PXdoNHQ1YXV0aDA=";
 wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTk3MTAwNDA/dGV4dD13aDR0NWF1dGgw";
@@ -8,3 +13,17 @@ wauthparam.tokencookiehourslifetime=18;
 wauthparam.redirect ="/dashboardinput"
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
+
+document.getElementById("examplePopup").addEventListener("click", () => {
+    Swal.fire({
+        footer: 'Letak kamera WhatsApp',
+        imageUrl: 'assets/img/camwab.jpg',
+        imageWidth: 400,
+        imageHeight: 300,
+        showCloseButton: true,
+        showConfirmButton: false,
+        customClass: {
+            popup: 'rounded-md',
+        }
+    });
+});
